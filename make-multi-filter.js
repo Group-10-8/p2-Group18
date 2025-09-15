f0412de78b1f8b9cc644d38439afbf93993e652f
Rishi Jinwala <rjinwal2@charlotte.edu>
2025-09-14 21:58:57 -0400

    commented make-multi-filter.js

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,19 +1,24 @@
 
-'use strict';
+'use strict'; //stricter handling in javascript
 
+//takes orginalarray as input
 function MakeMultiFilter(originalArray){
     const arrayFilterer = function (filterCriteria = undefined, callback = undefined){
         if (arrayFilterer.currentArray === undefined){
             arrayFilterer.currentArray = originalArray;
         }
+            
+        //makes sure orginalarray is actually an array
         if (Array.isArray(originalArray)) {
             if ((typeof filterCriteria === "function")) {
+                //filters out the elements 
                 arrayFilterer.currentArray =
                     arrayFilterer.currentArray.filter(e => filterCriteria(e));
-            } else {
+            } else { 
                 return arrayFilterer.currentArray;
             }
         }
+        //callback function if needed
         if (typeof callback === 'function'){
             callback.call(originalArray, arrayFilterer.currentArray);
         }
